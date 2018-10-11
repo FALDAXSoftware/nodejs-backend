27545e65cf07ae895d6862d4450e0d9b2551f3d6
kalpit <kalpit.akhawat@openxcell.info>
2018-10-11 10:49:19 +0530

    unknown

--- api/services/UploadFiles.js
+++ api/services/UploadFiles.js
@@ -1,4 +1,4 @@
-var gm = require('gm').subClass({imageMagick: true}); //Server
+var gm = require('gm').subClass({ imageMagick: true }); //Server
 // var gm = require('gm'); //Local
 var request = require('request');
 var fs = require('fs');
@@ -8,12 +8,12 @@ var s3 = new AWS.S3();
 AWS
   .config
   .loadFromPath('json/aws_config.json');
-var s3 = new AWS.S3({signatureVersion: 'v4'});
+var s3 = new AWS.S3({ signatureVersion: 'v4' });
 var mime = require('mime');
 
 function UploadFiles() {
 
-  return {upload: _upload};
+  return { upload: _upload };
 
   function _upload(filePath, name, uploadFileName) {
     return new Promise((resolve, reject) => {
@@ -23,13 +23,13 @@ function UploadFiles() {
 
       gm(filePath).noProfile().stream(function (err, stdout, stderr) {
         var buf = new Buffer('');
-        
+
         stdout.on('data', function (data) {
           buf = Buffer.concat([buf, data]);
         });
-       
+
         stdout.on('end', function (data) {
-           console.log("buf",buf);
+          console.log("buf", buf);
           var profile = {
             Bucket: 'varshalteamprivatebucket',
             Key: name + '/profile/' + uploadFileName,
