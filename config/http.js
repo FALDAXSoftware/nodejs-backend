bf48f5ec24e0aa910e0ec0647c4b6b0218604d52
Krina Soni <krina.soni@openxcellinc.com>
2018-10-30 08:48:16 +0530

    image upload limit

--- config/http.js
+++ config/http.js
@@ -49,21 +49,11 @@ module.exports.http = {
     *                                                                          *
     ***************************************************************************/
 
-    // bodyParser: (function _configureBodyParser(){
-    //   var skipper = require('skipper');
-    //   var middlewareFn = skipper({ strict: true });
-    //   return middlewareFn;
-    // })(),
-    bodyParser: (function () {
-      var opts = { limit: '50mb' };
-      var fn;
-
-      // Default to built-in bodyParser:
-      fn = require('skipper');
-      return fn(opts);
-
-    })
-
+    bodyParser: (function _configureBodyParser() {
+      var skipper = require('skipper');
+      var middlewareFn = skipper({ strict: true, limit: '50mb' });
+      return middlewareFn;
+    })(),
   },
 
 };
