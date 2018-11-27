fcdfbe56895e8e9cf74d06a095caf7f83ec29945
Krina Soni <krina.soni@openxcellinc.com>
2018-11-27 14:19:56 +0530

    route changes

--- config/routes.js
+++ config/routes.js
@@ -162,7 +162,7 @@ module.exports.routes = {
   'post /users/add-kyc-details': "KYCController.updateKYCInfo",
   'post /users/add-kyc-docs': "KYCController.uploadKYCDoc",
   'get /users/get-kyc-detail': 'KYCController.getKYCDetails',
-  'post /callbacktest': 'RootController.callbackTest',
+  'post /callbacktest': 'KYCController.callbackTest',
 
   //Static Page routes
   'get /users/static-page/:page': 'Statics.getStaticPage',
