29c703e84a3b822484460fc419ca281d2c515c84
Krina Soni <krina.soni@openxcellinc.com>
2018-11-28 08:54:46 +0530

    kyc details added for IDM

--- api/helpers/kycpic-upload.js
+++ api/helpers/kycpic-upload.js
@@ -15,18 +15,20 @@ module.exports = {
 
     fn: async function (inputs, exits) {
         let kyc_details = await KYC.findOne({ id: 56 });
+        let kycUploadDetails = {};
+
         console.log('finded kyc_details', kyc_details)
         countryData.forEach(function (item) {
             Object.keys(item).forEach(function (key) {
                 if (kyc_details.country == key) {
-                    kyc_details.docCountry = item[key];
-                    kyc_details.bco = item[key];
+                    kycUploadDetails.docCountry = item[key];
+                    kycUploadDetails.bco = item[key];
                 }
             });
         });
         await image2base64('https://s3.ap-south-1.amazonaws.com/varshalteamprivatebucket/' + kyc_details.front_doc)
             .then((response) => {
-                kyc_details.scanData = response;
+                kycUploadDetails.scanData = response;
             }).catch(
                 (error) => {
                     console.log('error', error);
@@ -34,7 +36,7 @@ module.exports = {
 
         await image2base64('https://s3.ap-south-1.amazonaws.com/varshalteamprivatebucket/' + kyc_details.back_doc)
             .then((response) => {
-                kyc_details.backsideImageData = response;
+                kycUploadDetails.backsideImageData = response;
             }).catch(
                 (error) => {
                     console.log('error', error);
@@ -47,20 +49,27 @@ module.exports = {
         } else if (kyc_details.id_type == 3) {
             kycDocType = 'ID';
         } else {
-            kyc_details.ssn = kyc_details.ssn;
+            kycUploadDetails.ssn = kyc_details.ssn;
         }
-        kyc_details.man = kyc_details.first_name + ' ' + kyc_details.last_name;
-        kyc_details.docType = kycDocType;
-        kyc_details.man = kyc_details.first_name + ' ' + kyc_details.last_name;
+        kycUploadDetails.man = kyc_details.first_name + ' ' + kyc_details.last_name;
+        kycUploadDetails.docType = kycDocType;
+        kycUploadDetails.man = kyc_details.first_name + ' ' + kyc_details.last_name;
+        kycUploadDetails.bfn = kyc_details.first_name;
+        kycUploadDetails.bln = kyc_details.last_name;
+        kycUploadDetails.bln = kyc_details.last_name;
+        kycUploadDetails.bsn = kyc_details.address + ' ' + kyc_details.address_2;
+        kycUploadDetails.bc = kyc_details.city;
+        kycUploadDetails.bz = kyc_details.zip;
+        kycUploadDetails.dob = kyc_details.dob;
 
-        console.log('kyc_details>>', kyc_details)
+        console.log('kyc_details>>', kycUploadDetails)
 
         request.post({
             headers: {
                 'Authorization': 'Basic ZmFsZGF4OjcxN2MzNGQ5NmRkNzA2N2JkYTAwMDFlMjlmZDk2MTlkYTMzYTk5ODM='
             },
             url: 'https://staging.identitymind.com/im/account/consumer',
-            json: kyc_details
+            json: kycUploadDetails
         }, async function (error, response, body) {
             try {
                 //console.log('error', error);
