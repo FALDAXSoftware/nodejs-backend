4eb0be79dd0f31741d35ddb30dea387233c27483
Kalpit <kalpit.akhawat@openxcell.info>
2018-12-19 08:41:14 +0530

    error handling under development

--- api/controllers/CoinsController.js
+++ api/controllers/CoinsController.js
@@ -6,282 +6,304 @@
  */
 
 module.exports = {
-  //---------------------------Web Api------------------------------
-  getAllCoins: async function (req, res) {
-    try {
-      let { page, limit, data } = req.allParams();
-      if (data) {
-        var balanceRes = await sails.sendNativeQuery("SELECT coins.*, wallets.balance, wallets.placed_balance FROM coins LEFT JOIN wal" +
-          "lets ON coins.id = wallets.coin_id WHERE (coins.coin_name LIKE '%" + data + "%' OR coins.coin_code LIKE '%" + data + "%') AND coins.is_active = true AND coins.deleted_at IS NULL ORDER BY wallets.bal" +
-          "ance DESC LIMIT " + limit + " OFFSET " + (limit * (page - 1)));
+    //---------------------------Web Api------------------------------
+    getAllCoins: async function (req, res) {
+        try {
+            let { page, limit, data } = req.allParams();
+            if (data) {
+                var balanceRes = await sails.sendNativeQuery("SELECT coins.*, wallets.balance, wallets.placed_balance FROM coins LEFT JOIN wal" +
+                    "lets ON coins.id = wallets.coin_id WHERE (coins.coin_name LIKE '%" + data + "%' OR coins.coin_code LIKE '%" + data + "%') AND coins.is_active = true AND coins.deleted_at IS NULL ORDER BY wallets.bal" +
+                    "ance DESC LIMIT " + limit + " OFFSET " + (limit * (page - 1)));
 
-        let allCoinsCount = await Coins.count({
-          where: {
-            deleted_at: null,
-            is_active: true,
-            or: [
-              {
-                coin_name: {
-                  contains: data
+                let allCoinsCount = await Coins.count({
+                    where: {
+                        deleted_at: null,
+                        is_active: true,
+                        or: [
+                            {
+                                coin_name: {
+                                    contains: data
+                                }
+                            }, {
+                                coin_code: {
+                                    contains: data
+                                }
+                            }
+                        ]
+                    }
+                });
+                if (balanceRes) {
+                    return res.json({
+                        "status": 200,
+                        "message": sails.__("Coin list"),
+                        "data": balanceRes,
+                        allCoinsCount
+                    });
                 }
-              }, {
-                coin_code: {
-                  contains: data
+            } else {
+                var balanceRes = await sails.sendNativeQuery(`SELECT coins.*, wallets.balance, wallets.placed_balance FROM coins LEFT JOIN wallets ON coins.id = wallets.coin_id WHERE coins.is_active = true AND coins.deleted_at IS NULL ORDER BY wallets.balance DESC LIMIT ${limit} OFFSET ${page} `);
+
+                let allCoinsCount = await Coins.count({
+                    where: {
+                        deleted_at: null,
+                        is_active: true
+                    }
+                });
+
+                if (balanceRes) {
+                    return res.json({
+                        "status": 200,
+                        "message": sails.__("Coin list"),
+                        "data": balanceRes,
+                        allCoinsCount
+                    });
                 }
-              }
-            ]
-          }
-        });
-        if (balanceRes) {
-          return res.json({
-            "status": 200,
-            "message": sails.__("Coin list"),
-            "data": balanceRes,
-            allCoinsCount
-          });
-        }
-      } else {
-        var balanceRes = await sails.sendNativeQuery(`SELECT coins.*, wallets.balance, wallets.placed_balance FROM coins LEFT JOIN wallets ON coins.id = wallets.coin_id WHERE coins.is_active = true AND coins.deleted_at IS NULL ORDER BY wallets.balance DESC LIMIT ${limit} OFFSET ${page} `);
-
-        let allCoinsCount = await Coins.count({
-          where: {
-            deleted_at: null,
-            is_active: true
-          }
-        });
-
-        if (balanceRes) {
-          return res.json({
-            "status": 200,
-            "message": sails.__("Coin list"),
-            "data": balanceRes,
-            allCoinsCount
-          });
-        }
-      }
-    } catch (err) {
-      console.log('>>>>err', err);
-      res
-        .status(500)
-        .json({
-          status: 500,
-          "err": sails.__("Something Wrong")
-        });
-      return;
-    }
-  },
-
-  //Create Wallet
-  createWallet: async function (req, res) {
-    let { coin_id } = req.allParams();
-    var requestedCoin = await Coins.find({ id: coin_id, deleted_at: null, is_active: true });
-    await sails
-      .helpers
-      .wallet
-      .create(requestedCoin.coin_code);
-    return res.json({
-      "status": 200,
-      "message": "wallet created",
-    });
-  },
-
-  // create all wallet
-  createAllWallet: async function (req, res) {
-    await sails.helpers.wallet.createAll();
-    return res.json({
-      "status": 200,
-      "message": "wallet created",
-    });
-  },
-
-  //-------------------------------CMS Api--------------------------
-  getCoins: async function (req, res) {
-    // req.setLocale('en')
-    let { page, limit, data } = req.allParams();
-    if (data) {
-      let coinsData = await Coins.find({
-        where: {
-          deleted_at: null,
-          or: [
-            {
-              coin_name: {
-                contains: data
-              }
-            }, {
-              coin_code: {
-                contains: data
-              }
             }
-          ]
+        } catch (err) {
+            console.log('>>>>err', err);
+            res
+                .status(500)
+                .json({
+                    status: 500,
+                    "err": sails.__("Something Wrong")
+                });
+            return;
         }
-      })
-        .sort("id ASC")
-        .paginate(page - 1, parseInt(limit));
-      let CoinsCount = await Coins.count({
-        where: {
-          deleted_at: null,
-          or: [
-            {
-              coin_name: {
-                contains: data
-              }
-            }, {
-              coin_code: {
-                contains: data
-              }
-            }
-          ]
-        }
-      });
-      if (coinsData) {
+    },
+
+    //Create Wallet
+    createWallet: async function (req, res) {
+        let { coin_id } = req.allParams();
+        var requestedCoin = await Coins.find({ id: coin_id, deleted_at: null, is_active: true });
+        await sails
+            .helpers
+            .wallet
+            .create(requestedCoin.coin_code);
         return res.json({
-          "status": 200,
-          "message": sails.__("Coin list"),
-          "data": coinsData,
-          CoinsCount
+            "status": 200,
+            "message": "wallet created",
         });
-      }
-    } else {
-      let coinsData = await Coins
-        .find({
-          where: {
-            deleted_at: null
-          }
-        })
-        .sort("id ASC")
-        .paginate(page - 1, parseInt(limit));
-      let CoinsCount = await Coins.count({
-        where: {
-          deleted_at: null
-        }
-      });
-      if (coinsData) {
-        return res.json({
-          "status": 200,
-          "message": sails.__("Coin list"),
-          "data": coinsData,
-          CoinsCount
-        });
-      }
-    }
-  },
+    },
 
-  create: async function (req, res) {
-    try {
-      if (req.body.coin_name && req.body.coin_code && req.body.limit && req.body.wallet_address) {
-        let existingCoin = await Coins.find({
-          deleted_at: null,
-          or: [
-            {
-              coin_name: req.body.coin_name
-            }, {
-              coin_code: req.body.coin_code
+    // create all wallet
+    createAllWallet: async function (req, res) {
+
+        try {
+            let result = await sails.helpers.wallet.createAll().tolerate("unkown", (err) => {
+                console.log("unkown------");
+                throw err;
+            });
+            console.log("result", result);
+
+            return res.json({
+                "status": 200,
+                "message": "wallet created",
+            });
+        } catch (error) {
+            // console.log("error", error);
+            if (error.raw) {
+                return res.status(500).json({
+                    status: 500,
+                    "err": error.raw.err
+                });
+            } else {
+                return res.status(500).json({
+                    status: 500,
+                    "err": sails.__("Something Wrong")
+                });
             }
-          ]
-        });
-        if (existingCoin.length > 0) {
-          res
-            .status(400)
-            .json({ "status": 400, "err": "Coin name or code already in use." });
-          return;
+
         }
-        var coins_detail = await Coins
-          .create({ coin_name: req.body.coin_name, coin_code: req.body.coin_code, limit: req.body.limit, wallet_address: req.body.wallet_address, created_at: new Date() })
-          .fetch();
-        if (coins_detail) {
-          //Send verification email in before create
-          res.json({ "status": 200, "message": "Coin created successfully." });
-          return;
+    },
+
+    //-------------------------------CMS Api--------------------------
+    getCoins: async function (req, res) {
+        // req.setLocale('en')
+        let { page, limit, data } = req.allParams();
+        if (data) {
+            let coinsData = await Coins.find({
+                where: {
+                    deleted_at: null,
+                    or: [
+                        {
+                            coin_name: {
+                                contains: data
+                            }
+                        }, {
+                            coin_code: {
+                                contains: data
+                            }
+                        }
+                    ]
+                }
+            })
+                .sort("id ASC")
+                .paginate(page - 1, parseInt(limit));
+            let CoinsCount = await Coins.count({
+                where: {
+                    deleted_at: null,
+                    or: [
+                        {
+                            coin_name: {
+                                contains: data
+                            }
+                        }, {
+                            coin_code: {
+                                contains: data
+                            }
+                        }
+                    ]
+                }
+            });
+            if (coinsData) {
+                return res.json({
+                    "status": 200,
+                    "message": sails.__("Coin list"),
+                    "data": coinsData,
+                    CoinsCount
+                });
+            }
         } else {
-          res
-            .status(400)
-            .json({ "status": 400, "err": "Something went wrong" });
-          return;
+            let coinsData = await Coins
+                .find({
+                    where: {
+                        deleted_at: null
+                    }
+                })
+                .sort("id ASC")
+                .paginate(page - 1, parseInt(limit));
+            let CoinsCount = await Coins.count({
+                where: {
+                    deleted_at: null
+                }
+            });
+            if (coinsData) {
+                return res.json({
+                    "status": 200,
+                    "message": sails.__("Coin list"),
+                    "data": coinsData,
+                    CoinsCount
+                });
+            }
         }
-      } else {
-        res
-          .status(400)
-          .json({ "status": 400, "err": "coin id is not sent" });
-        return;
-      }
-    } catch (error) {
-      res
-        .status(500)
-        .json({
-          status: 500,
-          "err": sails.__("Something Wrong")
-        });
-      return;
-    }
-  },
+    },
 
-  update: async function (req, res) {
-    try {
+    create: async function (req, res) {
+        try {
+            if (req.body.coin_name && req.body.coin_code && req.body.limit && req.body.wallet_address) {
+                let existingCoin = await Coins.find({
+                    deleted_at: null,
+                    or: [
+                        {
+                            coin_name: req.body.coin_name
+                        }, {
+                            coin_code: req.body.coin_code
+                        }
+                    ]
+                });
+                if (existingCoin.length > 0) {
+                    res
+                        .status(400)
+                        .json({ "status": 400, "err": "Coin name or code already in use." });
+                    return;
+                }
+                var coins_detail = await Coins
+                    .create({ coin_name: req.body.coin_name, coin_code: req.body.coin_code, limit: req.body.limit, wallet_address: req.body.wallet_address, created_at: new Date() })
+                    .fetch();
+                if (coins_detail) {
+                    //Send verification email in before create
+                    res.json({ "status": 200, "message": "Coin created successfully." });
+                    return;
+                } else {
+                    res
+                        .status(400)
+                        .json({ "status": 400, "err": "Something went wrong" });
+                    return;
+                }
+            } else {
+                res
+                    .status(400)
+                    .json({ "status": 400, "err": "coin id is not sent" });
+                return;
+            }
+        } catch (error) {
+            res
+                .status(500)
+                .json({
+                    status: 500,
+                    "err": sails.__("Something Wrong")
+                });
+            return;
+        }
+    },
 
-      const coin_details = await Coins.findOne({ id: req.body.coin_id });
-      if (!coin_details) {
-        return res
-          .status(401)
-          .json({ status: 401, err: 'Invalid coin' });
-      }
-      let existingCoin = await Coins.find({
-        deleted_at: null,
-        id: {
-          '!=': req.body.coin_id
-        },
-        or: [
-          {
-            coin_name: req.body.coin_name
-          }, {
-            coin_code: req.body.coin_code
-          }
-        ]
-      });
-      if (existingCoin.length > 0) {
-        res
-          .status(400)
-          .json({ "status": 400, "err": "Coin name or code already in use." });
-        return;
-      }
-      var coinData = {
-        id: req.body.coin_id,
-        ...req.body
-      }
-      var updatedCoin = await Coins
-        .update({ id: req.body.coin_id })
-        .set(req.body)
-        .fetch();
-      if (!updatedCoin) {
-        return res.json({ "status": 200, "message": "Something went wrong! could not able to update coin details" });
-      }
-      return res.json({ "status": 200, "message": "Coin details updated successfully" });
-    } catch (error) {
-      res
-        .status(500)
-        .json({
-          status: 500,
-          "err": sails.__("Something Wrong")
-        });
-      return;
-    }
-  },
+    update: async function (req, res) {
+        try {
 
-  delete: async function (req, res) {
-    let { id } = req.allParams();
-    if (!id) {
-      res
-        .status(500)
-        .json({ "status": 500, "err": "Coin id is not sent" });
-      return;
+            const coin_details = await Coins.findOne({ id: req.body.coin_id });
+            if (!coin_details) {
+                return res
+                    .status(401)
+                    .json({ status: 401, err: 'Invalid coin' });
+            }
+            let existingCoin = await Coins.find({
+                deleted_at: null,
+                id: {
+                    '!=': req.body.coin_id
+                },
+                or: [
+                    {
+                        coin_name: req.body.coin_name
+                    }, {
+                        coin_code: req.body.coin_code
+                    }
+                ]
+            });
+            if (existingCoin.length > 0) {
+                res
+                    .status(400)
+                    .json({ "status": 400, "err": "Coin name or code already in use." });
+                return;
+            }
+            var coinData = {
+                id: req.body.coin_id,
+                ...req.body
+            }
+            var updatedCoin = await Coins
+                .update({ id: req.body.coin_id })
+                .set(req.body)
+                .fetch();
+            if (!updatedCoin) {
+                return res.json({ "status": 200, "message": "Something went wrong! could not able to update coin details" });
+            }
+            return res.json({ "status": 200, "message": "Coin details updated successfully" });
+        } catch (error) {
+            res
+                .status(500)
+                .json({
+                    status: 500,
+                    "err": sails.__("Something Wrong")
+                });
+            return;
+        }
+    },
+
+    delete: async function (req, res) {
+        let { id } = req.allParams();
+        if (!id) {
+            res
+                .status(500)
+                .json({ "status": 500, "err": "Coin id is not sent" });
+            return;
+        }
+        let coinData = await Coins
+            .update({ id: id })
+            .set({ deleted_at: new Date() })
+            .fetch();
+        if (coinData) {
+            return res
+                .status(200)
+                .json({ "status": 200, "message": "Coin deleted successfully" });
+        }
     }
-    let coinData = await Coins
-      .update({ id: id })
-      .set({ deleted_at: new Date() })
-      .fetch();
-    if (coinData) {
-      return res
-        .status(200)
-        .json({ "status": 200, "message": "Coin deleted successfully" });
-    }
-  }
 };
--- api/helpers/wallet/create-all.js
+++ api/helpers/wallet/create-all.js
@@ -12,39 +12,52 @@ module.exports = {
 
     success: {
       description: 'All done.'
+    },
+    unknown: {
+      description: 'Something Went wrong'
     }
   },
 
   fn: async function (inputs, exits) {
-    console.log(sails.config.local.BITGO_ENV_MODE);
-    console.log(sails.config.local.BITGO_ACCESS_TOKEN);
+    return exits.unknown({
+      "err": sails.__("Something Wrong"),
+    });
+    try {
+      console.log(sails.config.local.BITGO_ENV_MODE);
+      console.log(sails.config.local.BITGO_ACCESS_TOKEN);
 
-    var bitgo = new BitGoJS.BitGo({ env: sails.config.local.BITGO_ENV_MODE, accessToken: sails.config.local.BITGO_ACCESS_TOKEN });
-    var requestedCoin = await Coins.find({ deleted_at: null, is_active: true })
+      var bitgo = new BitGoJS.BitGo({ env: sails.config.local.BITGO_ENV_MODE, accessToken: sails.config.local.BITGO_ACCESS_TOKEN });
+      var requestedCoin = await Coins.find({ deleted_at: null, is_active: true })
 
-    for (let index = 0; index < requestedCoin.length; index++) {
-      const coin = requestedCoin[index];
-      bitgo
-        .coin(coin.coin_code)
-        .wallets()
-        .generateWallet({
-          label: coin.coin_code + '-wallet',
-          passphrase: sails.config.local.BITGO_PASSPHRASE,
-          // enterprise: sails.config.local.BITGO_ENTERPRISE
-        })
-        .then(async newWallet => {
-          console.log(newWallet.wallet.id());
-          await Coins
-            .update({ id: coin.id })
-            .set({
-              'wallet_address': newWallet
-                .wallet
-                .id()
-            });
+      for (let index = 0; index < requestedCoin.length; index++) {
+        const coin = requestedCoin[index];
+        bitgo
+          .coin(coin.coin_code)
+          .wallets()
+          .generateWallet({
+            label: coin.coin_code + '-wallet',
+            passphrase: sails.config.local.BITGO_PASSPHRASE,
+            // enterprise: sails.config.local.BITGO_ENTERPRISE
+          })
+          .then(async newWallet => {
+            console.log(newWallet.wallet.id());
+            await Coins
+              .update({ id: coin.id })
+              .set({
+                'wallet_address': newWallet
+                  .wallet
+                  .id()
+              });
 
-        })
+          })
+      }
+      return exits.success();
+    } catch (error) {
+      return exits.serverError({
+        "status": 500,
+        "message": sails.__("Something Wrong"),
+      });
     }
-    return exits.success();
   }
 
 };
