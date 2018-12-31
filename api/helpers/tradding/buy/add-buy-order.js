647416d0f6d09fdae6ab47ad085691af9428d72c
Mansi <mansi@apple.local>
2018-12-31 15:45:27 +0530

    Limit order fees implementation

--- api/helpers/tradding/buy/add-buy-order.js
+++ api/helpers/tradding/buy/add-buy-order.js
@@ -22,9 +22,32 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     let orderData = inputs.buyLimitOrderData;
+    var currency = orderData.currency;
+    var crypto = orderData.settle_currency;
+    var total_price = orderData.limit_price * orderData.quantity;
     let buyAdded = await buyBook
       .create(orderData)
       .fetch();
+
+    var walletBalance = await sails
+      .helpers
+      .utilities
+      .getWalletBalance(orderData.crypto, orderData.currency, inputs.user_id)
+      .intercept("coinNotFound", () => {
+        return new Error("coinNotFound");
+      })
+      .intercept("serverError", () => {
+        return new Error("serverError");
+      });
+
+    var balance = walletBalance.placed_balance;
+    var updatedBalance = balance - total_price;
+    var updatedBalance = parseFloat((updatedBalance).toFixed(6));
+
+    var walletUpdate = Wallet
+      .update({id: walletBalance.id})
+      .set({placed_balance: updatedBalance});
+
     return exits.success(buyAdded);
   }
 };
