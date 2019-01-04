54f504ab16be95b180255a5080a68bcca4c22462
Kalpit <kalpit.akhawat@openxcell.info>
2019-01-04 11:20:52 +0530

    depth chart

---
+++ api/helpers/chart/get-depth-chart-detail.js
@@ -0,0 +1,66 @@
+module.exports = {
+
+
+  friendlyName: 'Get depth chart detail',
+
+
+  description: '',
+
+
+  inputs: {
+    crypto: {
+      type: 'string',
+      example: 'BTC',
+      description: 'Code of Crypto.',
+      required: true
+    },
+    currency: {
+      type: 'string',
+      example: 'ETH',
+      description: 'Code of Currency.',
+      required: true
+    },
+  },
+
+
+  exits: {
+
+    success: {
+      outputFriendlyName: 'Depth chart detail',
+    },
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    // Get depth chart detail.
+    var depthChartDetail;
+    // TODO
+
+    var buyDetails = await buyBook.find({
+      where: {
+        deleted_at: null,
+        settle_currency: inputs.crypto,
+        currency: inputs.currency
+      },
+      sort: 'price DESC'
+    })
+
+    var sellDetails = await sellBook.find({
+      where: {
+        deleted_at: null,
+        settle_currency: inputs.crypto,
+        currency: inputs.currency
+      },
+      sort: 'price ASC'
+    });
+
+    // Send back the result through the success exit.
+    return depthChartDetail;
+
+  }
+
+
+};
+
