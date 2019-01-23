5879ffba560944d80387dda3c7742533bf42f42a
Kalpit <kalpit.akhawat@openxcell.info>
2019-01-23 15:36:35 +0530

    auth change

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -30,6 +30,8 @@ module.exports = {
                 }
             }
         } catch (error) {
+            console.log(error);
+
             return res.status(500).json({ "status": 500, "err": sails.__("Something Wrong") });
         }
     },
