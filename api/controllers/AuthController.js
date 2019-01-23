2e86b7be11537943498525e992664119d6b705de
Krina Soni <krina.soni@openxcellinc.com>
2019-01-23 09:43:26 +0530

    Wallet cron started

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -20,10 +20,10 @@ module.exports = {
                         last_name: user.last_name
                     });
                     //   Create Recive Address
-                    // await sails
-                    //     .helpers
-                    //     .wallet
-                    //     .receiveAddress(user_detail);
+                    await sails
+                        .helpers
+                        .wallet
+                        .receiveAddress(user_detail);
                     return res.json({ "status": 200, "message": sails.__('Verify User') });
                 } else {
                     return res.status(400).json({ "status": 400, "err": sails.__('Invalid Token') });
