3d54b4c83c72227789ad25156c53344e1534a18d
Mansi <mansi@mini.local>
2019-04-02 10:44:36 +0530

    API key changes

--- api/models/referral.js
+++ api/models/referral.js
@@ -1,47 +1,51 @@
 /**
- * Jobs.js
+ * Referral.js
  *
- * @description :: A model definition.  Represents a database of jobs.
+ * @description :: A model definition.  Represents a database of referral.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
 
 module.exports = {
-    tableName: 'referral',
-    attributes: {
-        user_id: {
-            type: 'string',
-            columnName: 'user_id',
-        },
-        coin_id: {
-            type: 'string',
-            columnName: 'coin_id'
-        },
-        coin_name: {
-            model: 'coins',
-            columnName: 'coin_name'
-        },
-        created_at: {
-            type: 'ref',
-            columnType: 'datetime',
-            columnName: 'created_at'
-        },
-        updated_at: {
-            type: 'ref',
-            columnType: 'datetime',
-            columnName: 'updated_at'
-        },
-        deleted_at: {
-            type: 'ref',
-            columnType: 'datetime',
-            columnName: 'deleted_at'
-        }
+  tableName: 'referral',
+  attributes: {
+    user_id: {
+      type: 'string',
+      columnName: 'user_id'
     },
-    beforeCreate: function (val, next) {
-        val.created_at = new Date();
-        next();
+    coin_id: {
+      type: 'string',
+      columnName: 'coin_id'
     },
-    beforeUpdate: function (val, next) {
-        val.updated_at = new Date();
-        next();
+    coin_name: {
+      model: 'coins',
+      columnName: 'coin_name'
+    },
+    amount: {
+      type: 'number',
+      columnName: 'amount'
+    },
+    created_at: {
+      type: 'ref',
+      columnType: 'datetime',
+      columnName: 'created_at'
+    },
+    updated_at: {
+      type: 'ref',
+      columnType: 'datetime',
+      columnName: 'updated_at'
+    },
+    deleted_at: {
+      type: 'ref',
+      columnType: 'datetime',
+      columnName: 'deleted_at'
     }
+  },
+  beforeCreate: function (val, next) {
+    val.created_at = new Date();
+    next();
+  },
+  beforeUpdate: function (val, next) {
+    val.updated_at = new Date();
+    next();
+  }
 };
