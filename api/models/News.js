d371b6bc01540b64e207d1056c37b3029e4297ff
Mansi <mansi@mini.local>
2019-04-08 16:21:00 +0530

    fee and news model changes

--- api/models/News.js
+++ api/models/News.js
@@ -6,71 +6,71 @@
  */
 
 module.exports = {
-    tableName: 'news',
-    attributes: {
-        title: {
-            type: 'string',
-            columnName: 'title',
-        },
-        search_keywords: {
-            type: 'string',
-            columnName: 'search_keywords',
-        },
-        description: {
-            type: 'string',
-            columnName: 'description',
-        },
-        cover_image: {
-            type: 'string',
-            columnName: 'cover_image',
-        },
-        owner: {
-            type: 'string',
-            columnName: 'owner',
-        },
-        link: {
-            type: 'string',
-            columnName: 'link',
-        },
-        posted_at: {
-            type: 'ref',
-            columnType: 'datetime',
-            columnName: 'posted_at'
-        },
-        created_at: {
-            type: 'ref',
-            columnType: 'datetime',
-            columnName: 'created_at'
-        },
-        updated_at: {
-            type: 'ref',
-            columnType: 'datetime',
-            columnName: 'updated_at'
-        },
-        deleted_at: {
-            type: 'ref',
-            columnType: 'datetime',
-            columnName: 'deleted_at'
-        },
-        is_active: {
-            type: 'boolean',
-            columnName: 'is_active',
-            defaultsTo: true,
-        }
+  tableName: 'news',
+  attributes: {
+    title: {
+      type: 'string',
+      columnName: 'title'
     },
+    search_keywords: {
+      type: 'string',
+      columnName: 'search_keywords'
+    },
+    description: {
+      type: 'string',
+      columnName: 'description'
+    },
+    cover_image: {
+      type: 'string',
+      columnName: 'cover_image'
+    },
+    owner: {
+      type: 'string',
+      columnName: 'owner'
+    },
+    link: {
+      type: 'string',
+      columnName: 'link'
+    },
+    posted_at: {
+      type: 'ref',
+      columnType: 'datetime',
+      columnName: 'posted_at'
+    },
+    created_at: {
+      type: 'ref',
+      columnType: 'datetime',
+      columnName: 'created_at'
+    },
+    updated_at: {
+      type: 'ref',
+      columnType: 'datetime',
+      columnName: 'updated_at'
+    },
+    deleted_at: {
+      type: 'ref',
+      columnType: 'datetime',
+      columnName: 'deleted_at'
+    },
+    is_active: {
+      type: 'boolean',
+      columnName: 'is_active',
+      defaultsTo: true
+    }
+  },
 
-    customToJSON: function () {
-        if (!this.cover_image || this.cover_image == "" || this.cover_image == null) {
-            this.cover_image = sails.config.urlconf.CMS_URL + "blog/default_blog.png"
-        }
-        return this;
-    },
-    beforeCreate: (values, next) => {
-        values.created_at = new Date();
-        next();
-    },
-    beforeUpdate: (values, next) => {
-        values.updated_at = new Date();
-        next();
-    },
+  customToJSON: function () {
+    if (!this.cover_image || this.cover_image == "" || this.cover_image == null) {
+      this.cover_image = sails.config.urlconf.CMS_URL + "blog/default_blog.png"
+    }
+    return this;
+  },
+  beforeCreate: (values, next) => {
+    values.created_at = new Date();
+    next();
+  },
+  beforeUpdate: (values, next) => {
+    values.updated_at = new Date();
+    next();
+  }
 };
