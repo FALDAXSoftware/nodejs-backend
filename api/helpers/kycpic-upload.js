14ca74ff46167f7bf97f500e568e67aa61ecb071
Krina Soni <krina.soni@openxcellinc.com>
2019-04-18 08:36:10 +0530

    changes

--- api/helpers/kycpic-upload.js
+++ api/helpers/kycpic-upload.js
@@ -89,36 +89,36 @@ module.exports = {
         }
         kycUploadDetails.bc = kyc_details.city;
         kycUploadDetails.bz = kyc_details.zip;
-        kycUploadDetails.docCountry = 'US';
+        //kycUploadDetails.docCountry = 'US';
         kycUploadDetails.dob = moment(kyc_details.dob, 'DD-MM-YYYY').format('YYYY-MM-DD');
 
         console.log('kycUploadDetails FINAL', kycUploadDetails)
-        // request.post({
-        //     headers: {
-        //         'Authorization': 'Basic ZmFsZGF4OjcxN2MzNGQ5NmRkNzA2N2JkYTAwMDFlMjlmZDk2MTlkYTMzYTk5ODM='
-        //     },
-        //     url: 'https://staging.identitymind.com/im/account/consumer',
-        //     json: kycUploadDetails
-        // }, async function (error, response, body) {
-        //     try {
-        //         kyc_details.direct_response = response.body.res;
-        //         kyc_details.webhook_response = null;
-        //         await KYC.update({ id: kyc_details.id }).set({
-        //             direct_response: response.body.res,
-        //             webhook_response: null,
-        //             mtid: response.body.mtid,
-        //             comments: response.body.frd,
-        //             status: true,
-        //         });
-        //     } catch (error) {
-        //         console.log('error', error);
-        //         await KYC.update({ id: kyc_details.id }).set({
-        //             direct_response: "MANUAL_REVIEW",
-        //             webhook_response: "MANUAL_REVIEW",
-        //             comments: "Could Not Verify",
-        //             status: true,
-        //         });
-        //     }
-        // });
+        request.post({
+            headers: {
+                'Authorization': 'Basic ZmFsZGF4OjcxN2MzNGQ5NmRkNzA2N2JkYTAwMDFlMjlmZDk2MTlkYTMzYTk5ODM='
+            },
+            url: 'https://staging.identitymind.com/im/account/consumer',
+            json: kycUploadDetails
+        }, async function (error, response, body) {
+            try {
+                kyc_details.direct_response = response.body.res;
+                kyc_details.webhook_response = null;
+                await KYC.update({ id: kyc_details.id }).set({
+                    direct_response: response.body.res,
+                    webhook_response: null,
+                    mtid: response.body.mtid,
+                    comments: response.body.frd,
+                    status: true,
+                });
+            } catch (error) {
+                console.log('error', error);
+                await KYC.update({ id: kyc_details.id }).set({
+                    direct_response: "MANUAL_REVIEW",
+                    webhook_response: "MANUAL_REVIEW",
+                    comments: "Could Not Verify",
+                    status: true,
+                });
+            }
+        });
     }
 }
