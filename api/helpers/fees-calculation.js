c7ce5e38275b2368cf62c38fd585577c059c471c
Mansi <mansi@mini.local>
2019-09-10 10:42:58 +0530

    fees calculatio JST

---
+++ api/helpers/fees-calculation.js
@@ -0,0 +1,102 @@
+module.exports = {
+
+  friendlyName: 'Fees calculation',
+
+  description: 'Calculation of the Fees for JST',
+
+
+  inputs: {
+    coin_code: {
+      type: 'string',
+      example: 'btc',
+      description: 'symbol of the coin',
+      required: true
+    },
+    quantity: {
+      type: 'string',
+      example: '1',
+      description: 'Amount of the coin',
+      required: false
+    },
+    price: {
+      type: 'string',
+      example: '25.25',
+      description: 'Price of the coin',
+      required: false
+    }
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'All done.',
+    },
+
+  },
+
+
+  fn: async function (inputs, exits) {
+    try {
+      var coin = inputs.coin;
+      var feesValue;
+      if (coin == 'btc') {
+        var data = await AdminSetting.findOne({
+          where: {
+            deleted_at: null,
+            slug: 'btc_fee'
+          }
+        });
+
+        feesValue = ((inputs.quantity) / (25 * data.value));
+      } else if (coin == 'bch') {
+
+        var data = await AdminSetting.findOne({
+          where: {
+            deleted_at: null,
+            slug: 'bch_fees'
+          }
+        });
+
+        feesValue = ((data.value * inputs.quantity) / inputs.price)
+      } else if (coin == 'eth') {
+
+        var data = await AdminSetting.findOne({
+          where: {
+            deleted_at: null,
+            slug: 'eth_fees'
+          }
+        });
+
+        feesValue = (data.value * 21000);
+
+      } else if (coin == 'ltc') {
+
+        var data = await AdminSetting.findOne({
+          where: {
+            deleted_at: null,
+            slug: 'ltc_fees'
+          }
+        });
+
+        feesValue = data.value;
+
+      } else if (coin == 'xrp') {
+
+        var data = await AdminSetting.findOne({
+          where: {
+            deleted_at: null,
+            slug: 'xrp_fees'
+          }
+        });
+
+        feesValue = data.value
+      }
+
+      return exits.success(feesValue);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+};
