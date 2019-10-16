5afaf318e911b595ddd1c0c17fe0fea0e17e24e3
Mansi <mansi@mini.local>
2019-10-16 09:23:07 +0530

    thresold notification changes

--- api/helpers/notification/check-theresold-notification.js
+++ api/helpers/notification/check-theresold-notification.js
@@ -26,64 +26,64 @@ module.exports = {
       }
     }).sort('id DESC').limit(7)
 
-    // for (let index = 0; index < user.length; index++) {
-    //   const element = user[index];
-    //   var assetValue = element.asset;
+    for (let index = 0; index < user.length; index++) {
+      const element = user[index];
+      var assetValue = element.asset;
 
-    //   for (var j = 0; j < assetValue.length; j++) {
-    //     for (var k = 0; k < priceValue.length; k++) {
-    //       if (priceValue[k].coin_id == assetValue[j].coin_id) {
-    //         if (assetValue[j].upper_limit != undefined && assetValue[j].upper_limit != null) {
-    //           if (priceValue[k].quote.USD.price >= assetValue[j].upper_limit) {
-    //             var userData = await Users.findOne({
-    //               where: {
-    //                 id: element.user_id,
-    //                 is_active: true,
-    //                 deleted_at: null,
-    //                 is_verified: true
-    //               }
-    //             });
-    //             if (userData) {
-    //               if (assetValue[j].is_email_notification == true || assetValue[j].is_email_notification == "true") {
-    //                 if (userData.email != undefined) {
-    //                   await sails.helpers.notification.send.email("thresold_notification", userData)
-    //                 }
-    //               }
-    //               if (assetValue[j].is_sms_notification == true || assetValue[j].is_sms_notification == "true") {
-    //                 if (userData.phone_number != undefined)
-    //                   await sails.helpers.notification.send.text("thresold_notification", userData)
-    //               }
-    //             }
-    //           }
-    //         }
-    //         if (assetValue[j].lower_limit != undefined && assetValue[j].lower_limit != null) {
-    //           if (priceValue[k].quote.USD.price <= assetValue[j].lower_limit) {
-    //             var userData = await Users.findOne({
-    //               where: {
-    //                 id: element.user_id,
-    //                 is_active: true,
-    //                 deleted_at: null,
-    //                 is_verified: true
-    //               }
-    //             });
-    //             if (userData) {
-    //               if (assetValue[j].is_email_notification == true || assetValue[j].is_email_notification == "true") {
-    //                 if (userData.email != undefined) {
-    //                   await sails.helpers.notification.send.email("thresold_notification", userData)
-    //                 }
-    //               }
-    //               if (assetValue[j].is_sms_notification == true || assetValue[j].is_sms_notification == "true") {
-    //                 if (userData.phone_number != undefined)
-    //                   await sails.helpers.notification.send.text("thresold_notification", userData)
-    //               }
-    //             }
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
+      for (var j = 0; j < assetValue.length; j++) {
+        for (var k = 0; k < priceValue.length; k++) {
+          if (priceValue[k].coin_id == assetValue[j].coin_id) {
+            if (assetValue[j].upper_limit != undefined && assetValue[j].upper_limit != null) {
+              if (priceValue[k].quote.USD.price >= assetValue[j].upper_limit) {
+                var userData = await Users.findOne({
+                  where: {
+                    id: element.user_id,
+                    is_active: true,
+                    deleted_at: null,
+                    is_verified: true
+                  }
+                });
+                if (userData) {
+                  if (assetValue[j].is_email_notification == true || assetValue[j].is_email_notification == "true") {
+                    if (userData.email != undefined) {
+                      await sails.helpers.notification.send.email("thresold_notification", userData)
+                    }
+                  }
+                  if (assetValue[j].is_sms_notification == true || assetValue[j].is_sms_notification == "true") {
+                    if (userData.phone_number != undefined)
+                      await sails.helpers.notification.send.text("thresold_notification", userData)
+                  }
+                }
+              }
+            }
+            if (assetValue[j].lower_limit != undefined && assetValue[j].lower_limit != null) {
+              if (priceValue[k].quote.USD.price <= assetValue[j].lower_limit) {
+                var userData = await Users.findOne({
+                  where: {
+                    id: element.user_id,
+                    is_active: true,
+                    deleted_at: null,
+                    is_verified: true
+                  }
+                });
+                if (userData) {
+                  if (assetValue[j].is_email_notification == true || assetValue[j].is_email_notification == "true") {
+                    if (userData.email != undefined) {
+                      await sails.helpers.notification.send.email("thresold_notification", userData)
+                    }
+                  }
+                  if (assetValue[j].is_sms_notification == true || assetValue[j].is_sms_notification == "true") {
+                    if (userData.phone_number != undefined)
+                      await sails.helpers.notification.send.text("thresold_notification", userData)
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
 
-    // }
+    }
 
   }
 };
