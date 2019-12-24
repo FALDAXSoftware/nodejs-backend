ceed89eedc7c80c9d29d7ac205ce39ef2db457b7
jagdish.banda <jagdish.banda@openxcelltechnolabs.com>
2019-12-24 09:02:10 +0530

    logs changes

--- api/policies/isAdmin.js
+++ api/policies/isAdmin.js
@@ -150,6 +150,7 @@ module.exports = async function (req, res, next) {
           return next();
         }
       } else {
+        console.log("dsdsd");
         return res.status(403).json({
           status: 403,
           err: 'Unauthorized access'
@@ -159,6 +160,7 @@ module.exports = async function (req, res, next) {
 
     return next();
   } catch (err) {
+    console.log("DSDSDSDDSDSDD");
     console.log(err);
   }
 
