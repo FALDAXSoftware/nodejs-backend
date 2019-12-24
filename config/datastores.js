7e847ac18b4b9c32288b424cc4a0d85de90a4f3a
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2019-12-24 17:35:41 +0530

    messages changes and database pool size issue resolved

--- config/datastores.js
+++ config/datastores.js
@@ -56,6 +56,9 @@ module.exports.datastores = {
     // url: "postgresql://root:RXJSwBGZ7KdP9X4e@dev-faldax.cuhktbe1gpfj.us-east-2.rds.amazonaws.com/database",
     // url: "postgresql://faldaxdev:nBhAJbxd2LcpBjWJ@dev-faldax.cuhktbe1gpfj.us-east-2.rds.amazonaws.com/faldax",
     ssl: false,
+    pool: true,
+    connectionLimit: 10,
+    waitForConnections: true
     // pool: false
   },
 };
