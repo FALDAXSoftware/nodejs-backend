fbe03d7643c861f0a2c7574611f2cf9a2608e769
jagdish.banda <jagdish.banda@openxcelltechnolabs.com>
2020-01-08 11:21:18 +0530

    #3358

--- api/controllers/WalletController.js
+++ api/controllers/WalletController.js
@@ -1034,9 +1034,9 @@ module.exports = {
           walletUserData.send_address = walletData.send_address;
         }
 
-
+        console.log("walletUserData",walletUserData);
         if (walletUserData) {
-          if (walletUserData.receive_address === '') {
+          if (walletUserData.receive_address === '' || walletUserData.receive_address == null) {
             walletUserData['flag'] = 1;
           } else {
             walletUserData['flag'] = 0;
