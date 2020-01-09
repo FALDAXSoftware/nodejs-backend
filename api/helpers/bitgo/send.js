738675606b351f603a34f70f37506fdec017ddfd
Jagdish Banda <jagdish.banda@openxcelltechnolabs.com>
2020-01-09 03:47:48 +0000

    Update send.js

--- api/helpers/bitgo/send.js
+++ api/helpers/bitgo/send.js
@@ -50,25 +50,41 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     var access_token_value = await sails.helpers.getDecryptData(sails.config.local.BITGO_ACCESS_TOKEN);
-    var passphrase_value = await sails.helpers.getDecryptData(sails.config.local.BITGO_PASSPHRASE);
-    var enterprise_value = await sails.helpers.getDecryptData(sails.config.local.BITGO_ENTERPRISE);
-    console.log("---------------------WebhookOnReceive----------");
-    console.log("access_token_value", access_token_value);
-    console.log("passphrase_value", passphrase_value);
-    console.log("enterprise_value", enterprise_value);
-    var obj = {
-        'cache-control': 'no-cache',
-        Authorization: `Bearer ${access_token_value}`,
-        'Content-Type': 'application/json'
+    // var passphrase_value = await sails.helpers.getDecryptData(sails.config.local.BITGO_PASSPHRASE);
+    var passphrase_value;
+    var coinData = await Coins.findOne({
+      where: {
+        deleted_at: null,
+        is_active: true,
+        coin_code: inputs.coin
       }
-    console.log("obj",obj);
-    var bb ={
-        address: inputs.address,
-        amount: parseFloat(inputs.amount),
-        walletPassphrase: passphrase_value,
-        enterprise: enterprise_value
-      };
-    console.log("bb",bb);  
+    })
+    console.log("coinData",coinData);
+    if (coinData && coinData != undefined) {
+      if (inputs.coin == "btc") {
+        if (coinData.warm_wallet_address == inputs.walletId) {
+          passphrase_value = sails.config.local.BITGO_BTC_WARM_WALLET_PASSPHRASE;
+          console.log("In warm_wallet_address");
+        } else if (coinData.hot_send_wallet_address == inputs.walletId) {
+          passphrase_value = sails.config.local.BITGO_BTC_HOT_SEND_WALLET_PASSPHRASE;
+          console.log("In hot_send_wallet_address");
+        } else if (coinData.hot_receive_wallet_address == inputs.walletId) {
+          passphrase_value = sails.config.local.BITGO_BTC_HOT_RECEIVE_WALLET_PASSPHRASE;
+          console.log("In hot_receive_wallet_address");
+        } else if (coinData.custody_wallet_address == inputs.walletId) {
+          passphrase_value = sails.config.local.BITGO_PASSPHRASE;
+          console.log("In custody_wallet_address");
+        }
+      } else {
+        passphrase_value = sails.config.local.BITGO_PASSPHRASE;
+      }
+    } else {
+      passphrase_value = sails.config.local.BITGO_PASSPHRASE;
+    }
+
+    console.log("passphrase_value", passphrase_value);
+    var wallet_passphrase = await sails.helpers.getDecryptData(passphrase_value);
+    console.log("wallet_passphrase", wallet_passphrase);
     request({
       url: `${sails.config.local.BITGO_PROXY_URL}/${inputs.coin}/wallet/${inputs.walletId}/sendcoins`,
       method: "POST",
@@ -80,22 +96,20 @@ module.exports = {
       body: {
         address: inputs.address,
         amount: parseFloat(inputs.amount),
-        walletPassphrase: passphrase_value
+        walletPassphrase: wallet_passphrase
       },
       json: true
     }, function (err, httpResponse, body) {
       if (err) {
-        console.log("Error in wb:",err)
+        console.log("Error", err)
         return exits.error(err);
       }
-      console.log("body",body);
-      console.log("----------");
+      console.log("Res Body", body);
       if (body.error) {
         return exits.error(body);
       }
       return exits.success(body);
     });
   }
-
 
 };
