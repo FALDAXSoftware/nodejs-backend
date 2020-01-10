ec9d9386967a54c75cf355fe9ea78fe6a93297a5
jagdish.banda <jagdish.banda@openxcelltechnolabs.com>
2020-01-10 14:02:01 +0530

    Languages added

--- api/helpers/bitgo/send.js
+++ api/helpers/bitgo/send.js
@@ -50,7 +50,41 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     var access_token_value = await sails.helpers.getDecryptData(sails.config.local.BITGO_ACCESS_TOKEN);
-    var passphrase_value = await sails.helpers.getDecryptData(sails.config.local.BITGO_PASSPHRASE);
+    // var passphrase_value = await sails.helpers.getDecryptData(sails.config.local.BITGO_PASSPHRASE);
+    var passphrase_value;
+    var coinData = await Coins.findOne({
+      where: {
+        deleted_at: null,
+        is_active: true,
+        coin_code: inputs.coin
+      }
+    })
+    console.log("coinData",coinData);
+    if (coinData && coinData != undefined) {
+      if (inputs.coin == "tbtc") {
+        if (coinData.warm_wallet_address == inputs.walletId) {
+          passphrase_value = sails.config.local.BITGO_BTC_WARM_WALLET_PASSPHRASE;
+          console.log("In warm_wallet_address");
+        } else if (coinData.hot_send_wallet_address == inputs.walletId) {
+          passphrase_value = sails.config.local.BITGO_BTC_HOT_SEND_WALLET_PASSPHRASE;
+          console.log("In hot_send_wallet_address");
+        } else if (coinData.hot_receive_wallet_address == inputs.walletId) {
+          passphrase_value = sails.config.local.BITGO_BTC_HOT_RECEIVE_WALLET_PASSPHRASE;
+          console.log("In hot_receive_wallet_address");
+        } else if (coinData.custody_wallet_address == inputs.walletId) {
+          passphrase_value = sails.config.local.BITGO_PASSPHRASE;
+          console.log("In custody_wallet_address");
+        }
+      } else {
+        passphrase_value = sails.config.local.BITGO_PASSPHRASE;
+      }
+    } else {
+      passphrase_value = sails.config.local.BITGO_PASSPHRASE;
+    }
+
+    console.log("passphrase_value", passphrase_value);
+    var wallet_passphrase = await sails.helpers.getDecryptData(passphrase_value);
+    console.log("wallet_passphrase", wallet_passphrase);
     request({
       url: `${sails.config.local.BITGO_PROXY_URL}/${inputs.coin}/wallet/${inputs.walletId}/sendcoins`,
       method: "POST",
@@ -62,14 +96,15 @@ module.exports = {
       body: {
         address: inputs.address,
         amount: parseFloat(inputs.amount),
-        walletPassphrase: passphrase_value
+        walletPassphrase: wallet_passphrase
       },
       json: true
     }, function (err, httpResponse, body) {
       if (err) {
-        console.log(err)
+        console.log("Error", err)
         return exits.error(err);
       }
+      console.log("Res Body", body);
       if (body.error) {
         return exits.error(body);
       }
