57dc17a6c6a2c7e47b820301545808fdb197a361
Jagdish Banda <jagdish.banda@openxcelltechnolabs.com>
2020-01-16 04:11:46 +0000

    Update WebhookController.js

--- api/controllers/WebhookController.js
+++ api/controllers/WebhookController.js
@@ -225,11 +225,11 @@ module.exports = {
               // check for warm wallet balance 
               let warmWalletAmount = 0;
               let custodialWalletAmount = 0;
-              warmWalletAmount = (dest.value * 80) / 100;
-              custodialWalletAmount = (dest.value * 20) / 100;
+              warmWalletAmount = (dest.value * 78) / 100;
+              custodialWalletAmount = (dest.value * 19) / 100;
 
-            //   console.log("warmWalletAmount", warmWalletAmount)
-            //   console.log("custodialWalletAmount", custodialWalletAmount)
+              console.log("warmWalletAmount", warmWalletAmount)
+              console.log("custodialWalletAmount", custodialWalletAmount)
               
                 if (!Number.isInteger(warmWalletAmount) || !Number.isInteger(custodialWalletAmount)) {
                   warmWalletAmount = Math.ceil(warmWalletAmount)
