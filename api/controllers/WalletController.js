c74b35c35c1d04446eff66b8587115199db4047f
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-01-27 15:28:11 +0530

    wallet changes and jst value changes

--- api/controllers/WalletController.js
+++ api/controllers/WalletController.js
@@ -569,7 +569,7 @@ module.exports = {
                               source_address: warmWalletData.receiveAddress.address,
                               destination_address: wallet.send_address,
                               user_id: user_id,
-                              amount: (amountValue),
+                              amount: parseFloat(amountValue / 1e8).toFixed(8),
                               transaction_type: 'send',
                               is_executed: true,
                               transaction_id: transaction.txid,
