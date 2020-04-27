065b8829b53870a51653d09b1a7328c05c16c218
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-04-27 11:30:32 +0530

    pax network fee issue

--- api/controllers/WalletController.js
+++ api/controllers/WalletController.js
@@ -3119,7 +3119,10 @@ module.exports = {
           coin_code: data.coin
         }
       })
-      if (coinData.coin_code != "SUSU" && coinData.coin_code != "txrp" && coinData.coin_code != 'xrp') {
+
+      console.log("coinData", coinData.iserc)
+      console.log(coinData.coin_code != "SUSU" && coinData.coin_code != "txrp" && coinData.coin_code != 'xrp' && coinData.iserc == false)
+      if (coinData.coin_code != "SUSU" && coinData.coin_code != "txrp" && coinData.coin_code != 'xrp' && coinData.iserc == false) {
         if (sails.config.local.TESTNET == 1) {
           var valid = WAValidator.validate(data.address, (coinData.coin_name).toLowerCase(), 'testnet');
         } else {
